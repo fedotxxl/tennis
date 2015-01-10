ccdfd28e16416f065a77aecbfe8a5e19d24a8c0a
fedor.belov <fbelov@gramant.ru>
2015-01-10 17:01:50 +0300

    required fields

--- admin/js/angular/admin.orders.js
+++ admin/js/angular/admin.orders.js
@@ -1,8 +1,8 @@
 angular.module('app').
-    controller('OrdersNewController', function($scope, orders, _api) {
+    controller('OrdersNewController', function ($scope, orders, _api) {
         $scope.orders = orders;
 
-        $scope.actionMessage = function(order, actionType) {
+        $scope.actionMessage = function (order, actionType) {
             if (order.priorAction == actionType) {
                 return "Вы уверены?";
             } else {
@@ -10,7 +10,7 @@ angular.module('app').
             }
         };
 
-        $scope.actionMessageByType = function(actionType) {
+        $scope.actionMessageByType = function (actionType) {
             if (actionType == "approve") {
                 return "Можем";
             } else if (actionType == "approve.partially") {
@@ -20,7 +20,7 @@ angular.module('app').
             }
         };
 
-        $scope.getActionButtonClass = function(actionType) {
+        $scope.getActionButtonClass = function (actionType) {
             if (actionType == "approve") {
                 return 'btn-success';
             } else if (actionType == "approve.partially") {
@@ -30,43 +30,55 @@ angular.module('app').
             }
         };
 
-        var doOrChangeActionStatus = function(order, acitonStatus, action) {
+        var doOrChangeActionStatus = function (order, acitonStatus, action) {
             if (order.priorAction == acitonStatus) {
-                if (action) {
-                    action();
-                } else {
-                    submitAnswer(order, acitonStatus);
+                var hasError = $scope.hasCommentError(order) || $scope.hasLocationError(order);
+                if (!hasError) {
+                    if (action) {
+                        action();
+                    } else {
+                        submitAnswer(order, acitonStatus);
+                    }
                 }
             } else {
                 order.priorAction = acitonStatus;
             }
         };
 
-        var submitAnswer = function(order, aciton) {
+        var submitAnswer = function (order, aciton) {
             order.submitting = true;
-            _api.submitAnswer(order, aciton).then(function() {
+            _api.submitAnswer(order, aciton).then(function () {
                 order.action = aciton;
                 order.submitting = false;
             });
         };
 
-        $scope.approve = function(order) {
+        $scope.approve = function (order) {
             doOrChangeActionStatus(order, 'approve');
         };
 
-        $scope.approvePartially = function(order) {
+        $scope.approvePartially = function (order) {
             doOrChangeActionStatus(order, 'approve.partially');
         };
 
-        $scope.reject = function(order) {
+        $scope.reject = function (order) {
             doOrChangeActionStatus(order, 'reject');
         };
 
-        $scope.getLocationLabels = function(locations) {
-            var answer = _.map(locations, function(id) {
+        $scope.getLocationLabels = function (locations) {
+            var answer = _.map(locations, function (id) {
                 return $scope.getLocation(id).label;
             });
 
             return answer.join(", ")
-        }
+        };
+
+        $scope.hasCommentError = function (order) {
+            return order.priorAction == 'approve.partially' && !order.answer.comment;
+        };
+
+        $scope.hasLocationError = function (order) {
+            return  (order.priorAction == 'approve' || order.priorAction == 'approve.partially') &&
+                (order.answer.locations == null || order.answer.locations.length == 0);
+        };
     });
